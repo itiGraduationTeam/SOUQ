3274ac0ee43435076f596ea933b7dd72b87eca87
Saharss75 <Sahar.rasahar21@gmail.com>
2021-04-10 01:30:46 +0300

    working on mainHeader(Header-navBar) && add side menu bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHeaderModule } from './components/main-header/main-header.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MainHeaderModule,
-    NgbModule,
-    FontAwesomeModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, MainHeaderModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
