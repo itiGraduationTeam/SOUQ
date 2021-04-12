46e8d00bcc7658e8a221e4d7d9b5bcc713e876b6
salma335 <engsalma335@gmail.com>
2021-04-12 20:46:07 +0200

    popular component

--- src/app/components/homepage/popular/popular.component.ts
+++ src/app/components/homepage/popular/popular.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/Shared/Interfaces/IProduct';
 import { ProductService } from 'src/Shared/Services/product.service';
+declare var $:any;
 
 @Component({
   selector: 'app-popular',
@@ -30,72 +31,73 @@ export class PopularComponent implements OnInit {
   loading:boolean =false
 
   ngOnInit(): void {
-  //   this.loading =true
-  //   this.productServ.getProduct().subscribe(
-  //     data => {
-  //       this.fashionProductList = data;
-  //       $(document).ready(function () {
+     this.loading =true
+    this.productServ.getFashionProduct().subscribe(
+      data => {
+        this.fashionProductList = data;
+        console.log("popular",this.fashionProductList);
+        $(document).ready(function () {
 
-  //         $('.fashion-slider').slick({
-  //           infinite: true,
-  //           slidesToShow: 5,
-  //           slidesToScroll: 5,
-  //           responsive: [
-  //             {
-  //               breakpoint: 1024,
-  //               settings: {
-  //                 slidesToShow: 5,
-  //                 slidesToScroll: 5,
-  //                 infinite: true,
-  //               }
-  //             },
-  //             {
-  //               breakpoint: 800,
-  //               settings: {
-  //                 slidesToShow: 5,
-  //                 slidesToScroll: 5,
-  //                 infinite: true,
-  //               }
-  //             },
-  //             {
-  //               breakpoint: 700,
-  //               settings: {
-  //                 slidesToShow: 4,
-  //                 slidesToScroll: 4,
-  //                 infinite: true,
-  //               }
-  //             },
-  //             {
-  //               breakpoint: 600,
-  //               settings: {
-  //                 slidesToShow: 3,
-  //                 slidesToScroll: 3,
-  //                 infinite: true,
+          $('.fashion-slider').slick({
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 5,
+                  slidesToScroll: 5,
+                  infinite: true,
+                }
+              },
+              {
+                breakpoint: 800,
+                settings: {
+                  slidesToShow: 5,
+                  slidesToScroll: 5,
+                  infinite: true,
+                }
+              },
+              {
+                breakpoint: 700,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 4,
+                  infinite: true,
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
 
-  //               }
-  //             },
-  //             {
-  //               breakpoint: 480,
-  //               settings: {
-  //                 slidesToShow: 2,
-  //                 slidesToScroll: 2,
-  //                 infinite: true,
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: true,
 
-  //               }
-  //             },
-  //             {
-  //               breakpoint: 320,
-  //               settings: {
-  //                 slidesToShow: 1,
-  //                 slidesToScroll: 1                }
-  //             }
-  //           ]
-  //         });
-  //       })
-  //       this.loading =false
-  //     },
-  //     err => this.errorMessage = err
-  //   );
+                }
+              },
+              {
+                breakpoint: 320,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1                }
+              }
+            ]
+          });
+        })
+        this.loading =false
+      },
+      err => this.errorMessage = err
+    );
 
   }
 }
