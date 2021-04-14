c634256b68af811fb3731722f15a28be63c45c4d
Saharss75 <Sahar.rasahar21@gmail.com>
2021-04-14 03:46:46 +0300

    hide header and footer in specific pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,18 @@ export class AppComponent {
   title = 'souq';
   scrollTop = 200;
   isVisible: boolean = false;
+  showHeader = true;
+  constructor(public router: Router) {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        console.log('this.router.url', this.router.url);
+        let pages = ['/login', '/signup'];
+        this.showHeader = pages.includes(this.router.url);
+      }
+    });
+  } // note you have to use Public because you are using it in html file too.
 
   onScroll(event: any) {
     this.isVisible = this.scrollTop < event.target.scrollTop;
-    // console.log('asdasdwd', this.hideNav);
   }
 }
