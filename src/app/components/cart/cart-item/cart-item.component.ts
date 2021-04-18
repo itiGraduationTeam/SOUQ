d573b6f15082249cee0d1096e940fecbf6959acf
sabrien96 <sabriensaber0@gmail.com>
2021-04-18 15:14:11 +0200

    add addToCart and DeleteFromCart functions

--- src/app/components/cart/cart-item/cart-item.component.ts
+++ src/app/components/cart/cart-item/cart-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/Shared/Services/cart.service';
 
 @Component({
   selector: 'app-cart-item',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-item.component.scss']
 })
 export class CartItemComponent implements OnInit {
-
-  constructor() { }
+  cartItems: any;
+  error: any;
+  constructor(private cartService:CartService) { }
 
   ngOnInit(): void {
+    this.getcarts(); 
   }
 
+  getcarts() {
+    this.cartService.getAllCarts().subscribe(
+      (cartItems) => {
+        this.cartItems = cartItems;
+        console.log("array of cart: ",this.cartItems)
+      },
+      (error) => this.error = error
+    )
+  }
+
+  deletCart( item:any) {
+    console.log("we try to delete")
+    this.cartService.deleteCart(item).subscribe(
+      (data) => {
+      
+        console.log("data")
+        this.error = ""
+      },
+      err => this.error = "error"
+
+    )
+  this.cartItems=  this.cartItems.filter((ele:any)=>ele.productId._id!=item);
+
+  }
 }
--- src/app/components/product-details/product-summary/product-summary.component.ts
+++ src/app/components/product-details/product-summary/product-summary.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { IOptions } from 'src/Shared/Interfaces/IOptions';
+import { CartService } from 'src/Shared/Services/cart.service';
 import { FilterService } from 'src/Shared/Services/filter.service';
 
 @Component({
@@ -9,7 +10,8 @@ import { FilterService } from 'src/Shared/Services/filter.service';
 })
 export class ProductSummaryComponent implements OnInit {
   @Input() currentProduct:any;
-  constructor(private filterService:FilterService) { }
+  error:any;
+  constructor(private filterService:FilterService,private cartService:CartService) { }
   option:IOptions[]=[{
     options:[],
     cateid:0,
@@ -21,18 +23,36 @@ export class ProductSummaryComponent implements OnInit {
   ngOnInit(): void {
     this.filterService.getAllSubcategoryByName(this.currentProduct.subcategory).subscribe(
       (data)=>{
-        console.log(data);
+        // console.log(data);
         this.option=data;
-         console.log("option");
-         console.log(data);
+        //  console.log("option");
+        //  console.log(data);
        this.optionArray=   this.option[0].options[0];
-       console.log(  "ditalis", this.optionArray);
+      //  console.log(  "ditalis", this.optionArray);
         var keys=Object.keys(this.optionArray);
       this.keyFilter =keys;
-        console.log("details key",this.keyFilter);
+        // console.log("details key",this.keyFilter);
     
       }
     );
+  }
+
+  addToCart() {
+    this.cartService.addToCart(this.currentProduct._id,1).subscribe(
+      (data) => {
+        console.log('_id :'+this.currentProduct._id)
+
+        // console.log(data)
+        // this.error = ""
+      },
+      err => {
+        alert("please login first")
+        
+        // console.warn("error : ",err);
+        
+      }
+
+    )
   }
 
 }
