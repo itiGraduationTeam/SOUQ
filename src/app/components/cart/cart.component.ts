524cc723644e287cba70bd83f75ddb93cf2ed0ca
sabrien96 <sabriensaber0@gmail.com>
2021-04-21 01:04:02 +0200

    design orders and address components

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartService } from 'src/Shared/Services/cart.service';
+import { LocalStorageService } from 'src/Shared/Services/local-storage.service';
 
 @Component({
   selector: 'app-cart',
@@ -11,7 +13,10 @@ export class CartComponent implements OnInit {
   error: any;
   totalPrice: any = 0;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService,
+    private localStorage:LocalStorageService,
+    private router:Router
+    ) { }
 
   ngOnInit(): void {
 
@@ -22,6 +27,27 @@ export class CartComponent implements OnInit {
 
    
   }
+  navigate(event:any){
+    if(event==='checkout')
+    {
+      // let token=this.localStorage.get("userToken")+"";
+      // console.log("token is: ",token);
 
+      // if(token){
+      //   console.log("token found");
+        
+        this.router.navigate(['/checkout'])
+      // }
+      // else{
+      //   console.log("token not found");
+
+      //   this.router.navigate(['/signUp'])
+
+      // }
+    }
+    else if(event==='signin'){
+      this.router.navigate(['/login'])
+    }
+  }
   
 }
