ee769537c990f663a3d5274eb7c98b6cd4fadde5
sabrien96 <sabriensaber0@gmail.com>
2021-04-24 02:53:10 +0200

    work on  add product but not finished

--- src/app/admin/admin-panel/admin-products/admin-products.component.ts
+++ src/app/admin/admin-panel/admin-products/admin-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FilterService } from 'src/Shared/Services/filter.service';
 import { ProductService } from 'src/Shared/Services/product.service';
 
 @Component({
@@ -8,86 +9,146 @@ import { ProductService } from 'src/Shared/Services/product.service';
   styleUrls: ['./admin-products.component.scss']
 })
 export class AdminProductsComponent implements OnInit {
-  productList:any;
-  isOpen=false;
-  loading=true;
-  openModal=false;
-  selectedProduct:any;
-  editForm=this.formBuilder.group({
-    name:['',Validators.required],
-    price:['',Validators.required],
-    discount:['',Validators.required]   
+  productList: any;
+  isOpen = false;
+  loading = true;
+  openModal = false;
+  selectedOption = "";
+  allCategories: any;
+  allsubCategories: any;
+  selectedProduct: any;
+  editForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    discount: ['', Validators.required]
+  });
+  addProductForm = this.formBuilder.group({
+    name: [''],
+    price: [''],
+    countInStock: [''],
+    discount: [''],
+    categories: [''],
+    subCategory: [''],
+    image:[''],
+    rating: [''],
+    description: [''],
   });
 
-  constructor(private productServ:ProductService, private formBuilder:FormBuilder) { }
+  constructor(private productServ: ProductService,
+    private filterServe: FilterService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-  this.getAllProduct();
+    this.getAllProduct();
+    this.getAllCategories();
   }
-  getAllProduct(){
+  ngOnChanges() {
+
+  }
+
+  getAllProduct() {
     this.productServ.getProduct().subscribe(
-      data=>{
-        this.productList=data;
-        this.loading=false;
+      data => {
+        this.productList = data;
+        this.loading = false;
         // console.log("all data: ",this.productList);
-        
+
       },
-      err=>{
-        console.log("err from admin-products: ",err);
-        
+      err => {
+        console.log("err from admin-products: ", err);
+
       }
     )
   }
 
   //remove product by name
-  removeProduct(proName:any){
+  removeProduct(proName: any) {
     this.productServ.removeProduct(proName).subscribe(
-      data=>{
-        console.log("result of remove product: ",data);     
+      data => {
+        console.log("result of remove product: ", data);
       },
-      err=>{
-        console.log("err from remove product: ",err);     
+      err => {
+        console.log("err from remove product: ", err);
       }
     )
-    console.log("from admin products: ",proName); 
+    console.log("from admin products: ", proName);
   }
-//remove product by name
-editProduct(){
-  let newData=this.editForm.value;
-  console.log("form value: ",newData);
-this.productServ.editProduct(this.selectedProduct._id,
-  newData.name,newData.price,newData.discount).subscribe(
-    data=>{
-      console.log("result of edit product: ",data);     
-    },
-    err=>{
-      console.log("err from edit product: ",err);     
-    }
-  )
-}
-openModalForm(prod:any){
-  this.selectedProduct=prod;
-  console.log("selected product: ",this.selectedProduct._id);     
+  //remove product by name
+  editProduct() {
+    let newData = this.editForm.value;
+    console.log("form value: ", newData);
+    this.productServ.editProduct(this.selectedProduct._id,
+      newData.name, newData.price, newData.discount).subscribe(
+        data => {
+          console.log("result of edit product: ", data);
+        },
+        err => {
+          console.log("err from edit product: ", err);
+        }
+      )
+  }
+  addProduct() {
+    let newData = this.addProductForm.value;
+    console.log("form value: ", newData);
+    this.productServ.addProduct(newData).subscribe(
+        data => {
+          console.log("result of add product: ", data);
+        },
+        err => {
+          console.log("err from add product: ", err);
+        }
+      )
+  }
 
-  this.openModal=true;
-}
-closeModal(){
-  this.openModal=false;
-}
-  //toggle flag to open or close add product form
-  openAddForm()
-  {
-    // this.isOpen=!this.isOpen;
-    if(this.isOpen)
-    {
-      console.log("if");
-      
-      this.isOpen=false;
+  // get categories and subCategories
+  getAllCategories() {
+    this.filterServe.getAllCategory().subscribe(
+      data => {
+        this.allCategories = data
+        console.log("data: ", this.allCategories);
+
+      },
+      err => console.log(err)
+    );
+
+  }
+  subCateList() {
+
+    if (this.selectedOption != "") {
+      let id = parseInt(this.selectedOption)
+      console.log("selected option id: ", id);
+      this.filterServe.getAllSubcategoryByCateId(id).subscribe(
+        data => {
+          this.allsubCategories = data;
+          console.log("selected option: ", this.allsubCategories);
+
+        },
+        err => console.log(err)
+      );
     }
-    else{
+
+
+  }
+  openModalForm(prod: any) {
+    this.selectedProduct = prod;
+    console.log("selected product: ", this.selectedProduct._id);
+    this.openModal = true;
+  }
+  closeModal() {
+    this.openModal = false;
+  }
+  //toggle flag to open or close add product form
+  openAddForm() {
+    // this.isOpen=!this.isOpen;
+    if (this.isOpen) {
+      console.log("if");
+
+      this.isOpen = false;
+    }
+    else {
       console.log("else");
 
-      this.isOpen=true;
+      this.isOpen = true;
 
     }
   }
